e6c9bf9875f4a0d742b4f28abc3c33084608f19e
Mayuri Chavan <mayurichavan66@gmail.com>
2022-03-14 19:01:04 +0530

    v13

---
+++ footer.php
@@ -0,0 +1,57 @@
+<footer class="ftrbg">
+		<br>
+		<div class="overlay"></div>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-3">
+				<img src="img/logo-ss.png" style="width:100%;">
+					<ul class="ftco-footer-social list-unstyled">
+						
+						<li><a href="https://www.facebook.com/Steelsoft/" target="_blank"><img src="img/footer/facebook.png" style="width:33px"></a></li>
+						<li><a href="https://www.linkedin.com/company/steelsoft-global/"  target="_blank"><img src="img/footer/linkedin.png" style="width:33px"></a>
+						</li>
+						<li><a href="https://www.instagram.com/steelsoftglobal/"  target="_blank"><img src="img/footer/instagram.png" style="width:33px"></a></li>
+						<li><a href="https://www.youtube.com/channel/UC38wDl4V58KvnOfSQGUnlLw" target="_blank"><img src="img/footer/youtube.png" style="width:33px"></a></li>
+			
+					</ul>
+				</div>
+				<div class="col-md-2">
+					<div class="ftco-footer-widget mb-4 ml-md-4">
+						<h3 class="ftco-heading-2">Links</h3>
+						<ul class="list-unstyled">
+							<li> <a href="index.php">Home</a></li>
+							<li> <a href="about.html">About</a></li>
+							<!-- <li><i class="bi bi-arrow-right-short"></i><a href="benefits.html">Benefits</a></li> -->
+							<li><a href="project.html">Projects</a></li>
+							<li><a href="services.html">Services</a></li>
+							<li><a href="faq.html">FAQ</a></li>
+							<li><a href="career.html">Career</a></li>
+						</ul>
+					</div>
+				</div>
+			
+				<div class="col-md-7">
+					<div class="d-flex flex-wrap justify-content-evenly">
+						<div class="" style="width:310px;">
+							<a href="https://goo.gl/maps/Xgi8TWMR9iCzhYD2A"  target="_blank"> <img style="width:300px; margin: 0px 0px 0px -29px;"src="img/steelsoft map.png" alt="..."/></a>
+						</div>
+					
+						<div class="" style="width:230px;">
+							<h3 class="ftco-heading-2">Location </h3>
+							<div class="block-23 mb-3">
+								<ul>
+									<li> <i class="bi bi-geo-alt-fill"></i> <span class="text"> Brahmavrunda Colony,<br>Flat No-301,Vihaan,Pimple Nilakh,<br>
+											Pune 4110027</span></li>
+									<li><a href="tel:853-019-1192"> <i class="bi bi-telephone-fill"></i><span
+											class="text">+918530191192</span></a></li>
+									<li><a href="mailto:info@steelsoft-global.in?subject=Mail from Our Site"> <i
+											class="bi bi-envelope-open-fill"></i> <span>info@steelsoft-global.in</span></a></li>
+
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
